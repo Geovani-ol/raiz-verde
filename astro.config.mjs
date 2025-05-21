5579b9148de962069f37aea7561233b41f1411e5
Geovani_Dev <davidolvera475@gmail.com>
2025-05-21 17:21:50 -0600

    Update F

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [react(), tailwind()]
 });
